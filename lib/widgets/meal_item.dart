eb28afc02526077cf927179777ff94cb11298695
M0alaa330 <mohassan08563@gmail.com>
2024-09-28 23:55:39 +0300

    romove inkwell and add navigaor to icon button "meal item"

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:meal_app/screens/meal_details.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem(
@@ -10,49 +9,45 @@ class MealItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        Navigator.pushNamed(context, "mealdetails", arguments: id);
-      },
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Container(
-          width: double.infinity,
-          height: 200,
-          decoration: BoxDecoration(
-            borderRadius:
-                const BorderRadius.only(topRight: Radius.circular(15)),
-            color: Colors.teal[300],
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                height: double.infinity,
-                width: 150,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                  image: AssetImage(image!),
-                  fit: BoxFit.fill,
-                )),
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Container(
+        width: double.infinity,
+        height: 150,
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.only(topRight: Radius.circular(15)),
+          color: Colors.teal[300],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              height: double.infinity,
+              width: 150,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                image: AssetImage(image!),
+                fit: BoxFit.fill,
+              )),
+            ),
+            Text(
+              name ?? 'Unnamed',
+              style: const TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "mealdetails", arguments: id);
+              },
+              icon: const Icon(
+                Icons.arrow_right_alt_outlined,
+                size: 30,
+                color: Colors.white,
               ),
-              Text(
-                name ?? 'Unnamed',
-                style: const TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.arrow_right_alt_outlined,
-                  size: 30,
-                  color: Colors.white,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
